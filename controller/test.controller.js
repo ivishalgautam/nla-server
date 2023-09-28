4bae0352973bc4c5faccf6ddd6e663f6fe7c9201
Vishal Gautam <76483761+ivishalgautam@users.noreply.github.com>
2023-09-28 11:47:06 +0530

    fixed time change

--- controller/test.controller.js
+++ controller/test.controller.js
@@ -297,7 +297,7 @@ async function getFilteredTests(req, res) {
   try {
     const tests = await pool.query(
       `SELECT * FROM tests WHERE grade = $1 AND subject = $2 AND test_type = 'olympiad';`,
-      [grade, subject]
+      [grade ?? "", subject ?? ""]
     );
     res.json(tests.rows);
   } catch (error) {
