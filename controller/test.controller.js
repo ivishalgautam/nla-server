95afb97ef5d91a5664d8fdf0b180e2e338a5302e
vishal <ivishalgautamm@gmail.com>
2023-10-06 19:11:29 +0530

    now admin can add name of the grade manually

--- controller/test.controller.js
+++ controller/test.controller.js
@@ -14,13 +14,13 @@ async function createTest(req, res) {
       instructions,
     } = req.body;
 
-    const sDate = new Date(start_time);
-    const eDate = new Date(end_time);
+    // const sDate = new Date(start_time);
+    // const eDate = new Date(end_time);
 
-    sDate.setMinutes(0);
-    sDate.setSeconds(0);
-    eDate.setMinutes(0);
-    eDate.setSeconds(0);
+    // sDate.setMinutes(0);
+    // sDate.setSeconds(0);
+    // eDate.setMinutes(0);
+    // eDate.setSeconds(0);
 
     // Convert to Indian Standard Time (IST)
     const startDateIST = moment(start_time).tz("Asia/Kolkata");
