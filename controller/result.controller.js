8b0d6de9d4c6b3fe63be5455da15bcf1dca70040
sahil dudhat <sahildudhat03@gmail.com>
2025-03-07 13:03:23 +0530

    Create get Api for student Result and ans sheet

--- controller/result.controller.js
+++ controller/result.controller.js
@@ -183,6 +183,71 @@ async function getStudentResults(req, res) {
     res.status(500).json({ message: error.message });
   }
 }
+async function getStudentResultsStu(req, res) {
+  const studentId = parseInt(req.params.studentId);
+  try {
+    const { rows } = await pool.query(
+      `
+      SELECT 
+            sr.*, 
+            s.id as student_id, 
+            s.fullname,
+            s.school_name,
+            g.name as class,
+            t.id as test_id, 
+            t.name as test_name,
+            t.test_type,
+            t.subject,
+            t.start_time as held_on,
+            COALESCE(json_agg(qs.answer ORDER BY qs.created_at), '[]') as right_answers
+        FROM
+            student_results as sr
+        JOIN
+            students as s ON sr.student_id = s.id
+        JOIN
+            tests as t on sr.test_id = t.id
+        JOIN
+            questions as qs on qs.test_id = t.id
+        JOIN grades as g on s.grade = g.id    
+        WHERE
+          sr.student_id = $1
+        GROUP BY
+          sr.id, s.id, g.name, t.id
+        ORDER BY created_at DESC
+        ;`,
+      [studentId]
+    );
+
+    const updatedResults = rows.map((item) => {
+      let studentPoints = 0;
+      let studentAttempted = 0;
+      let totalPoints = item.right_answers?.length;
+      let totalQuestions = item.right_answers?.length;
+
+      if (item.user_answers && item.right_answers) {
+        item.user_answers.forEach((answer, index) => {
+          if (answer !== null) studentAttempted += 1;
+          if (answer == item.right_answers[index]) {
+            studentPoints += 1;
+          }
+        });
+      }
+      return {
+        ...item,
+        student_points: studentPoints,
+        student_attempted: studentAttempted,
+        total_points: totalPoints,
+        total_questions: totalQuestions,
+        grade: calculateGrade(studentPoints, totalPoints, totalQuestions),
+      };
+    });
+
+    res.json(updatedResults);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: error.message });
+  }
+}
 
 async function getAnswerSheet(req, res) {
   const { studentId, testId } = req.params;
@@ -217,10 +282,45 @@ async function getAnswerSheet(req, res) {
     res.status(500).json({ message: error.message });
   }
 }
+async function getAnswerSheetStu(req, res) {
+  const { studentId, testId } = req.params;
+  const { t } = req.query;
+
+  try {
+    let studentAnswers;
+    if (studentId && testId && t) {
+      const answers = await pool.query(
+        "SELECT * FROM student_results WHERE student_id = $1 AND test_id = $2;",
+        [studentId, testId]
+      );
+
+      const matchingRows = answers.rows.filter((row) => {
+        const createdAt = new Date(row.created_at);
+        const tDate = new Date(t);
+        return createdAt.getTime() === tDate.getTime();
+      });
+
+      studentAnswers =
+        matchingRows.length > 0 ? matchingRows[0].user_answers : null;
+    }
+
+    const questions = await pool.query(
+      "SELECT * FROM questions WHERE test_id = $1 ORDER BY id ASC;",
+      [testId]
+    );
+
+    res.json({ questions: questions.rows, studentAnswers });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: error.message });
+  }
+}
 
 module.exports = {
   createResult,
   getResults,
   getStudentResults,
+  getStudentResultsStu,
   getAnswerSheet,
+  getAnswerSheetStu,
 };
