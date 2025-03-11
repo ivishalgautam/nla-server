3d5f8d79c68cf503f103bbf84f123fb338a3041f
sahil dudhat <sahildudhat03@gmail.com>
2025-03-11 14:50:52 +0530

    revert Add id in grade

--- controller/grade.controller.js
+++ controller/grade.controller.js
@@ -2,7 +2,7 @@ const { pool } = require("../config/db");
 
 async function createGrade(req, res) {
   try {
-    const { name ,id } = req.body;
+    const { name } = req.body;
     const adminId = req.user.id;
 
     const gradeExist = await pool.query(
@@ -16,8 +16,7 @@ async function createGrade(req, res) {
         .json({ message: `'${name}' already exists for this admin!` });
     }
 
-    await pool.query(`INSERT INTO grades (id, name, admin_id) VALUES ($1, $2, $3);`, [
-      id,
+    await pool.query(`INSERT INTO grades (name, admin_id) VALUES ($1, $2);`, [
       name,
       adminId,
     ]);
